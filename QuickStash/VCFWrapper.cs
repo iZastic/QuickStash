3b7f567aa075780dd5809d0365934f6ad63189fb
iZastic <izastic@gmail.com>
2023-07-08 05:38:04 -0600

    Forgot to add VCF wrapper

---
+++ QuickStash/VCFWrapper.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx.Unity.IL2CPP;
+using ProjectM.Network;
+using VampireCommandFramework;
+
+namespace QuickStash
+{
+    public static class VCFWrapper
+    {
+        public static bool Enabled
+        {
+            get
+            {
+                return IL2CPPChainloader.Instance.Plugins.ContainsKey("gg.deca.VampireCommandFramework");
+            }
+        }
+
+        public static void RegisterAll()
+        {
+            CommandRegistry.RegisterAll();
+        }
+
+
+        [Command("stash", shortHand: "cc", description: "Compulsively Count on all nearby allied chests.", adminOnly: false)]
+        public static void VCFCompulsivelyCount(ChatCommandContext ctx)
+        {
+            var fromChar = new FromCharacter() { User = ctx.Event.SenderUserEntity, Character = ctx.Event.SenderCharacterEntity };
+
+            if (QuickStashServer.MergeInventories(fromChar))
+                ctx.Reply("Stashed all items");
+            else
+                ctx.Reply("Failed to stash items. Contact admin or moderator for support.");
+        }
+    }
+}
